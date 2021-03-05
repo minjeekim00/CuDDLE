4e184ff8d727ae0331f38ac56e025303a6b2b9e0
jryoungw <et2035@naver.com>
2021-03-05 18:40:23 +0900

    Update barcode_example.py

--- barcode_example.py
+++ barcode_example.py
@@ -10,4 +10,4 @@ rf_f, rf_d = barcode_realfake.get_barcode()
 rr_f, rr_d = barcode_realreal.get_barcode()
 
 print("Mutual Fidelity   : {:.3f} | Mutual Diversity   : {:.3f}".format(rf_f, rf_d))
-print("Relative Fidelity : {:.3f} | Relative Diversity : {:.3f}".format(rf_f/rf_f, rf_d/rr_d))
+print("Relative Fidelity : {:.3f} | Relative Diversity : {:.3f}".format(rf_f/rr_f, rf_d/rr_d))
