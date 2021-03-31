59e5237dd3d2b74ef9bf4d28414ec613693fa859
jryoungw <et2035@naver.com>
2021-03-31 17:52:47 +0900

    Update barcode_example.py

--- barcode_example.py
+++ barcode_example.py
@@ -1,15 +1,9 @@
 import numpy as np
-import sklearn.metrics
-import matplotlib.pyplot as plt
+
 from barcode import Barcode
 
-barcode_realfake = Barcode(real, fake)
-barcode_realreal = Barcode(real, real)
-barcode_fakefake = Barcode(fake, fake)
+superior = np.load('./brain_superior_embs.npz')['distance'].squeeze()
+inferior = np.load('./brain_inferior_embs.npz')['distance'].squeeze()
 
-rf_f, rf_d = barcode_realfake.get_barcode()
-rr_f, rr_d = barcode_realreal.get_barcode()
-ff_f, ff_d = barcode_fakefake.get_barcode()
-
-print("Mutual Fidelity   : {:.3f} | Mutual Diversity   : {:.3f}".format(rf_f, rf_d))
-print("Relative Fidelity : {:.3f} | Relative Diversity : {:.3f}".format(rf_f/rr_f, rf_d/(np.sqrt(rr_d) * np.sqrt(ff_d))))
+barcode = Barcode(superior, inferior)
+barcode.get_barcode()
